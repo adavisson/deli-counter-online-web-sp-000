08eef7f95604cb87ae1ce9140796b2d3895b693c
Andrew Davisson <akdavisson4@gmail.com>
2019-06-09 23:43:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def line(queue)
+  if queue.length < 1
+    puts "The line is currently empty."
+  else
+    queue.each_with_index do |customer,idx|
+      puts "#{customer} is number #{idx+ + 1 in line}."
+    end
+  end
+end
