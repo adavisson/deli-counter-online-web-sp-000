81d96c34e9b954764ceba9ecbfbda799dedfb85e
Andrew Davisson <akdavisson4@gmail.com>
2019-06-09 23:44:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(queue)
     puts "The line is currently empty."
   else
     queue.each_with_index do |customer,idx|
-      puts "#{customer} is number #{idx+ + 1 in line}."
+      puts "#{customer} is number #{idx+ + 1} in line."
     end
   end
 end
